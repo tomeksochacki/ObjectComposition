fc6b3bf76494948083b45499c232a15891966bae
tomek <tomeksochacki@wp.pl>
2020-04-07 00:08:27 +0200

    Update

--- src/com/example/Ram.java
+++ src/com/example/Ram.java
@@ -1,6 +1,7 @@
 package com.example;
 
 public class Ram {
+    private Size size = new Size();
     public Ram(){
         System.out.println("Induced constructor 'Ram'.");
     }
---
+++ src/com/example/Size.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Size {
+    public Size(){
+        System.out.println("Induced constructor 'Size'.");
+    }
+}
